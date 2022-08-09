3ccad0ad7dc106184115dc203ed304b360d0af60
Josue-Santos-Pinto <js.pinto55@gmail.com>
2022-08-09 12:02:15 -0300

    Menu Drawer Completo

---
+++ src/screens/WallScreen/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export default {
+    Container: styled.SafeAreaView`
+        flex: 1;
+        background-color: #F5F6FA;
+    `,
+    Scroller: styled.ScrollView`
+        flex: 1;
+        padding: 20px;
+    `,
+    LoadingIcon: styled.ActivityIndicator``,
+   
+};
